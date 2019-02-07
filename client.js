c0761972fcf5b22f76dfce04cc185f21eb146264
KatiLong <katileighlong@gmail.com>
2019-02-06 19:47:23 -0700

    Updated numbers to concat, started calculate function to be called inside operation listener

--- client.js
+++ client.js
@@ -7,9 +7,9 @@
 const buttons = {
     firstRow: ['C', '/'],
     secondRow: [7, 8, 9, 'x'],
-    thirdRow: ['4', '5', '6', '-'],
-    fourthRow: ['1', '2', '3', '+'],
-    finalRow: ['0', '.', '=']
+    thirdRow: [4, 5, 6, '-'],
+    fourthRow: [1, 2, 3, '+'],
+    finalRow: [0, '.', '=']
 }
 const operations = {
     divide: (num, total) => total/num,
@@ -38,6 +38,15 @@ const generateButton = (buttons) => {
 // Updates displayed Total
 const updateTotal = () => document.getElementById("total").innerHTML=total;
 // IF total 0 & Number buttons pressed, add to total & Display it
+let currentOperator = '';
+
+const calculate = ({operations} = add, subtract, divide, multiply) => {
+    // calculate & display
+    switch (currentOperator) {
+        default:
+            console.log('No operator, retain total', total);
+    }
+}
 
 // operator functions - need way to validate number or other operator?
 // Clear => resets #total content to 0
@@ -56,12 +65,41 @@ const clear = () => {
 
 // Event Listeners
 let operators = document.getElementsByClassName("operator");
-let numbers = document.getElementsByClassName("operator");
+let numbers = document.getElementsByClassName("number");
 
-document.getElementById("C").addEventListener("click", (e) => console.log(e.target));
+document.getElementById("C").addEventListener("click", (e) => {
+    currentOperator = '';
+    total = 0;
+    updateTotal();
+    console.log(e.target, currentOperator, total);
+});
 document.getElementById(".").addEventListener("click", (e) => console.log(e.target));
-Array.from(operators).forEach((el) => el.addEventListener("click", (e) => console.log(e.target)));
-Array.from(numbers).forEach((el) => el.addEventListener("click", (e) => console.log(e.target)));
+Array.from(operators).forEach((el) => el.addEventListener("click", (e) => {
+    console.log(e.target);
+    // If new operator pressed & last thing pressed was operator, default to current operator
+    // If currentOperator is defined & current operator not Equals
+    if (e.target.id === "=") {
+        calculate();
+    } else if (currentOperator !== '' && e.target.id !== "=") {
+        calculate();
+        currentOperator = e.target.id;
+    }
+    
+}));
+Array.from(numbers).forEach((el) => el.addEventListener("click", (e) => {
+    console.log(e.target.id, total);
+    if (total == 0) { //if total is zero
+        total += parseInt(e.target.id);
+        updateTotal();
+    } else { //if total doesn't equal zero (and last button pressed a number) concat number string
+        total += e.target.id;
+        updateTotal();
+    }
+    // if (currentOperator === '') {
+    //     //concat number string
+    // }
+    console.log(total);
+}));
 // document.getElementById("x").addEventListener("click", (e) => console.log(e.target));
 // document.getElementById("-").addEventListener("click", (e) => console.log(e.target));
 // document.getElementById("+").addEventListener("click", (e) => console.log(e.target));
