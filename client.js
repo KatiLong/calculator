40a62ccc53d62f2b00c58686d7e6e8178b947686
KatiLong <katileighlong@gmail.com>
2019-02-08 17:21:23 -0700

    Working except for decimals

--- client.js
+++ client.js
@@ -12,10 +12,10 @@ const buttons = {
     finalRow: [0, '.', '=']
 }
 const operations = {
-    divide: (num, total) => total/num,
-    multiply: (num, total) => total*num,
-    subtraction: (num, total) => total -= num,
-    addition: (num, total) => total += num
+    divide: (num, total) => total/parseFloat(num),
+    multiply: (num, total) => total*parseFloat(num),
+    subtraction: (num, total) => total - parseFloat(num),
+    addition: (num, total) => total + parseFloat(num)
 }
 
 let total = 0;
@@ -26,7 +26,6 @@ let total = 0;
 const generateButton = (buttons) => {
     let calculatorHtml = '';
     for (let row in buttons) {
-        console.log(row);
         buttons[row].forEach((val, index, arr) => {
             if (index === arr.length-1) calculatorHtml += `<div class="${row} operator" id="${val}">${val}</div>`;
             else if (typeof(val) === "number") calculatorHtml += `<div class="${row}-${index} number" id="${val}">${val}</div>`;
@@ -36,17 +35,35 @@ const generateButton = (buttons) => {
     return calculatorHtml;
 }
 // Updates displayed Total
-const updateTotal = () => document.getElementById("total").innerHTML=total;
+const updateDisplay = (num) => document.getElementById("total").innerHTML=num;
 // IF total 0 & Number buttons pressed, add to total & Display it
 let currentOperator = '';
+let currentNum = 0;
 
-const calculate = ({operations} = add, subtract, divide, multiply) => {
+const calculate = () => {
+    console.log('calculate invoked', currentOperator);
     // calculate & display
     switch (currentOperator) {
+        case '/' :
+            console.log('divide');
+            total = operations.divide(currentNum, total);
+            break;
+        case 'x' :
+            console.log('multiple');
+            total = operations.multiply(currentNum, total);
+            break;
+        case '-' :
+            console.log('subtract');
+            total = operations.subtraction(currentNum, total);
+            break;
+        case '+' :
+            console.log('add');
+            total = operations.addition(currentNum, total);
+            break;
         default:
             console.log('No operator, retain total', total);
     }
-    updateTotal();
+    updateDisplay(total);
 }
 
 // operator functions - need way to validate number or other operator?
@@ -70,36 +87,53 @@ let numbers = document.getElementsByClassName("number");
 
 document.getElementById("C").addEventListener("click", (e) => {
     currentOperator = '';
+    currentNum = 0;
     total = 0;
-    updateTotal();
-    console.log(e.target, currentOperator, total);
+    updateDisplay(0);
+    console.log(currentOperator, total);
 });
-document.getElementById(".").addEventListener("click", (e) => console.log(e.target));
-Array.from(operators).forEach((el) => el.addEventListener("click", (e) => {
+document.getElementById(".").addEventListener("click", (e) => {
     console.log(e.target);
+    currentNum += '.';
+    updateDisplay(currentNum);
+});
+Array.from(operators).forEach((el) => el.addEventListener("click", (e) => {
     // If new operator pressed & last thing pressed was operator, default to current operator
-    // If currentOperator is defined & current operator not Equals
     if (e.target.id === "=") {
         calculate();
-    } else if (currentOperator !== '' && e.target.id !== "=") {
+        currentOperator = '';
+    } 
+    // If currentOperator is defined & current operator not Equals
+    else if (currentOperator !== '' && e.target.id !== "=") {
         calculate();
         currentOperator = e.target.id;
     }
-    
+    else {
+        currentOperator = e.target.id;
+    }
+    console.log('currentOperator: ', currentOperator);
 }));
 Array.from(numbers).forEach((el) => el.addEventListener("click", (e) => {
-    console.log(e.target.id, total);
-    if (total == 0) { //if total is zero
-        total += parseInt(e.target.id);
-        updateTotal();
+    if (total == 0) { // if total is zero
+        console.log('numbers if');
+        currentNum = parseFloat(e.target.id);
+        total = parseFloat(e.target.id);
+        updateDisplay(currentNum);
+    } else if (currentOperator !== ''){ // if an operator has been pressed
+        console.log('numbers else if');
+        currentNum = parseFloat(e.target.id);
+        // display current number
+        document.getElementById("total").innerHTML=e.target.id;
     } else { //if total doesn't equal zero (and last button pressed a number) concat number string
+        console.log('numbers else');
+        currentNum += e.target.id; 
         total += e.target.id;
-        updateTotal();
+        updateDisplay(currentNum);
     }
     // if (currentOperator === '') {
     //     //concat number string
     // }
-    console.log(total);
+    console.log('currentNum:', currentNum);
 }));
 // document.getElementById("x").addEventListener("click", (e) => console.log(e.target));
 // document.getElementById("-").addEventListener("click", (e) => console.log(e.target));
